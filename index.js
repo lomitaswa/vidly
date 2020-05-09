8e3743dc024da232832f0b634dbf2d712327126d
lomitaswa <lomitaswa.suna@hotmail.com>
2020-05-09 15:18:47 +0530

    Unit Testing done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('./startup/logging');
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config');
--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston'); 
-require('winston-mongodb');
+//require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -12,8 +12,8 @@ module.exports = function() {
     });
 
     winston.add(winston.transports.File, { filename: 'logfile.log' });
-    winston.add(winston.transports.MongoDB, {
-        db: 'mongodb://localhost/vidly',
-        level: 'info'
-    });
+    // winston.add(winston.transports.MongoDB, {
+    //     db: 'mongodb://localhost/vidly',
+    //     level: 'info'
+    // });
 }
