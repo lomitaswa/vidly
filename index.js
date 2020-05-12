16c3e4897222e099c61a9fd610fe81d675fe409a
lomitaswa <lomitaswa.suna@hotmail.com>
2020-05-12 11:45:46 +0530

    start added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config');
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`)); 
