13b872f63f037a417685482aa9bbf626dde227a6
SSBarik <ssbarik.sf@gmail.com>
2022-10-03 19:31:09 +0530

    refactor: middleware module to ES6+

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,6 @@
 const winston = require('winston');
 
-module.exports = function(err, req, res, next) {
+module.exports = (err, req, res, next) => {
     winston.error( err.message, err)
 
     res.status(500).send('Something has failed');
